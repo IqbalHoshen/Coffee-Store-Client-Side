4890f552792808b87897db362780e101c5c21cdc
Iqbal Hoshen Saif <iqbalhoshensaif@gmail.com>
2026-01-17 19:00:38 +0600

    Hosting Firebase

--- src/component/Follow_Instagram.jsx
+++ src/component/Follow_Instagram.jsx
@@ -1,4 +1,12 @@
 import Multi_Title from "./Multi_Title";
+import coffeeImage1 from "../assets/images/cups/Rectangle 9.png";
+import coffeeImage2 from "../assets/images/cups/Rectangle 10.png";
+import coffeeImage3 from "../assets/images/cups/Rectangle 11.png";
+import coffeeImage4 from "../assets/images/cups/Rectangle 12.png";
+import coffeeImage5 from "../assets/images/cups/Rectangle 13.png";
+import coffeeImage6 from "../assets/images/cups/Rectangle 14.png";
+import coffeeImage7 from "../assets/images/cups/Rectangle 15.png";
+import coffeeImage8 from "../assets/images/cups/Rectangle 16.png";
 
 const titleString = {
   subText: "Follow Us Now",
@@ -13,28 +21,28 @@ const Follow_Instagram = () => {
       </header>
       <main className="grid grid-cols-4 gap-5 my-16">
         <figure>
-          <img src="/src/assets/images/cups/Rectangle 9.png" alt="image" />
+          <img src={coffeeImage1} alt="image" />
         </figure>
         <figure>
-          <img src="/src/assets/images/cups/Rectangle 10.png" alt="image" />
+          <img src={coffeeImage2} alt="image" />
         </figure>
         <figure>
-          <img src="/src/assets/images/cups/Rectangle 11.png" alt="image" />
+          <img src={coffeeImage3} alt="image" />
         </figure>
         <figure>
-          <img src="/src/assets/images/cups/Rectangle 12.png" alt="image" />
+          <img src={coffeeImage4} alt="image" />
         </figure>
         <figure>
-          <img src="/src/assets/images/cups/Rectangle 13.png" alt="image" />
+          <img src={coffeeImage5} alt="image" />
         </figure>
         <figure>
-          <img src="/src/assets/images/cups/Rectangle 14.png" alt="image" />
+          <img src={coffeeImage6} alt="image" />
         </figure>
         <figure>
-          <img src="/src/assets/images/cups/Rectangle 15.png" alt="image" />
+          <img src={coffeeImage7} alt="image" />
         </figure>
         <figure>
-          <img src="/src/assets/images/cups/Rectangle 16.png" alt="image" />
+          <img src={coffeeImage8} alt="image" />
         </figure>
       </main>
     </div>
